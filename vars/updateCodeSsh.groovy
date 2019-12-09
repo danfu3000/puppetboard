56656e3e11abede5ecffd7196c7a73d852ba1285
danfu3000 <danfu3000@gmail.com>
2019-12-09 11:50:40 +0800

    test update ssh

--- vars/updateCodeSsh.groovy
+++ vars/updateCodeSsh.groovy
@@ -12,12 +12,7 @@ def call() {
         stages {
             stage('Pull') {
                 steps {
-                    sshPublisher failOnError: true, publishers: [sshPublisherDesc(configName: "${SSH_NAME}", transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'export BUILDDIR=${PROJECT_DIR};if [ ! -d ${BUILDDIR} ];then mkdir -p ${BUILDDIR}; cd ${BUILDDIR}; git init; git remote add origin ssh://git@github.com/megaease/${PROJECT_NAME}.git; fi;cd ${BUILDDIR};GIT_SSH_COMMAND="${GIT_SSH_COMMAND}" git pull', execTimeout: 600000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '${HOME}', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]
-                }
-            }
-            stage('Pullconfig') {
-                steps {
-                    sshPublisher failOnError: true, publishers: [sshPublisherDesc(configName: 'km05', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'export BUILDDIR=${PROJECT_DIR};export CONFIGDIR=${CONFIG_DIR};if [ ! -d ${CONFIGDIR} ];then mkdir -p ${CONFIGDIR}; cd ${CONFIGDIR}; git init; git remote add origin ssh://git@github.com/megaease/${CONFIG_NAME}.git; fi;cd ${CONFIGDIR};GIT_SSH_COMMAND="${GIT_SSH_CONFIG" git pull origin ${CONFIG_BRANCH}; cp *.env ${BUILDDIR}/scripts/', execTimeout: 600000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '${HOME}', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]
+                    sshPublisher failOnError: true, publishers: [sshPublisherDesc(configName: "${SSH_NAME}", transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'export BUILDDIR=${PROJECT_DIR};if [ ! -d ${BUILDDIR} ];then mkdir -p ${BUILDDIR}; cd ${BUILDDIR}; ls -l', execTimeout: 600000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '${HOME}', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]
                 }
             }
         }
